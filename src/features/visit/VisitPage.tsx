d2f5c8965b474ab8d770f1322b4c9b149158c73a
keeptrain <chefgilang2@gmail.com>
2026-08-20 12:04:33 +0700

    style: align visit page background with zinc palette

--- src/features/visit/VisitPage.tsx
+++ src/features/visit/VisitPage.tsx
@@ -2,7 +2,7 @@ import VisitForm from "./components/VisitForm";
 
 export default function VisitPage() {
   return (
-    <main className="flex-1 bg-emerald-50 py-10 sm:py-16">
+    <main className="flex-1 bg-zinc-50 pt-10 pb-16 ">
       <div className="mx-auto max-w-4xl space-y-8 px-4 sm:px-6 lg:px-8">
         {/* Main Visit Form */}
         <VisitForm />
